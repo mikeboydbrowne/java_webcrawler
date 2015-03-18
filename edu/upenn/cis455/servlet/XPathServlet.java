6b68b15ab14cfd3cc9edf7ac9fe9b7b212f500c3
Mike Browne <michaelboydbrowne@gmail.com>
2015-03-18 00:08:33 -0400

    Get form interface to show up

--- edu/upenn/cis455/servlet/XPathServlet.java
+++ edu/upenn/cis455/servlet/XPathServlet.java
@@ -1,5 +1,10 @@
 package edu.upenn.cis455.servlet;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 import javax.servlet.http.*;
 
 @SuppressWarnings("serial")
@@ -17,10 +22,22 @@ public class XPathServlet extends HttpServlet {
 	}
 
 	@Override
-	public void doGet(HttpServletRequest request, HttpServletResponse response) {
-		/* TODO: Implement user interface for XPath engine here */
-
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws FileNotFoundException {
 		
+		// Displaying the interface
+		File 			index = new File("workspace/HW2/WebContent/WEB-INF/index.html");
+		FileInputStream fileInput = new FileInputStream(index);
+		
+		int content;
+		try {
+			while ((content = fileInput.read()) != -1) {
+				response.getOutputStream().write(content);
+			}
+			fileInput.close(); // Closing the file stream
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 	}
 
 }
