aa2cea97b16d1c52dbe7035bbe78ab3436b5df8f
Mike Browne <michaelboydbrowne@gmail.com>
2015-03-30 02:15:58 -0400

    Finish HTTP crawler implementation

--- edu/upenn/cis455/crawler/XPathCrawler.java
+++ edu/upenn/cis455/crawler/XPathCrawler.java
@@ -13,7 +13,6 @@ import java.util.Date;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.regex.Pattern;
 
 import javax.net.ssl.HttpsURLConnection;
 import javax.xml.parsers.DocumentBuilder;
@@ -27,7 +26,6 @@ import org.w3c.tidy.Tidy;
 import org.xml.sax.SAXException;
 
 import edu.upenn.cis455.crawler.info.URLInfo;
-import edu.upenn.cis455.storage.CrawlerEntity;
 import edu.upenn.cis455.storage.DBWrapper;
 
 
@@ -85,8 +83,6 @@ public class XPathCrawler {
 		// while the queue is still full, keep crawling
 		while (!urlQueue.isEmpty()) {
 			String nextUrl = urlQueue.poll();
-			Queue<String> currentQueue = urlQueue;
-			HashSet<String> prev = previouslySearched;
 			currentUrl = nextUrl;
 			if (nextUrl.startsWith("https://")) {
 				currentProtocol = "https://";
@@ -102,25 +98,29 @@ public class XPathCrawler {
 
 	}
 
+	/**
+	 * Processes the url, adding links if html, checking xpath if xml
+	 * @param url - url to process
+	 * @throws UnknownHostException
+	 * @throws IOException
+	 */
 	private static void processUrl(String url) throws UnknownHostException,
 			IOException {
 		if (!previouslySearched.contains(url)) {
 			if (url.startsWith("https://")) {
 				processHttpsRequest(url);
 			} else {
-
 				// open up a socket and send a HEAD request
 				URLInfo currentURL = new URLInfo(url);
 				Socket headS = new Socket(currentURL.getHostName(), currentURL.getPortNo());
 				PrintWriter headOutput = new PrintWriter(headS.getOutputStream());
-
 				// sent first request to url
 				headOutput.println("HEAD " + currentURL.getFilePath() + " HTTP/1.1");
 				headOutput.println("Host: localhost");
 				headOutput.println("User-Agent: cis455crawler");
 				headOutput.println("");
 				headOutput.flush();
-
+				System.out.println("Checking crawlability of: " + url);
 				// get input
 				BufferedReader headInput = new BufferedReader(new InputStreamReader(headS.getInputStream()));
 				String res = "";
@@ -140,75 +140,60 @@ public class XPathCrawler {
 							return;
 					}
 				}
-
 				// closing the head socket
 				headOutput.close();
 				headInput.close();
 				headS.close();
-
-				System.out.println(res);
-
-				// if content type is right
-				if (checkContentTypeHttp(res) && checkSizeHttp(res) && checkModifiedHttp(url, res)) {
-
-					// opening up a socket for a GET request
-					Socket getS = new Socket(currentURL.getHostName(), currentURL.getPortNo());
-					PrintWriter getOutput = new PrintWriter(getS.getOutputStream());
-					
-
-					// send another request
-					getOutput.println("GET " + currentURL.getFilePath() + " HTTP/1.1");
-					getOutput.println("Host: localhost");
-					getOutput.println("User-Agent: cis455crawler");
-					getOutput.println("");
-					getOutput.flush();
-					
-					// Setting up JTidy
-					Tidy domParse = new Tidy();
-					domParse.setForceOutput(true);
-					domParse.setShowErrors(0);
-					domParse.setQuiet(true);
-					boolean isHTML = !contentType.contains("xml");
-					
-					// HTML Parsing
-					Document docTemp = null;
-					
-					// XML parsing
-					DocumentBuilderFactory builder = DocumentBuilderFactory.newInstance();
-					DocumentBuilder	dom = null;
-					
-					// parsing HTML
-					if (isHTML) {
-						docTemp = domParse.parseDOM(new BufferedReader(new InputStreamReader(getS.getInputStream())), null);
-						
-						// searching document
-						searchForUrls(docTemp);
-						
-						// adding to store
-						dbInstance.putDocument(url, docTemp);
-						dbInstance.updateTime(url, currentDate.getTime());
-					
-					// parsing XML
-					} else {
-						try {
-							dom = builder.newDocumentBuilder();
-							docTemp = dom.parse(getS.getInputStream());
-						} catch (ParserConfigurationException e) {
-							e.printStackTrace();
-						} catch (SAXException e) {
-							e.printStackTrace();
-						}
-						
-						// EVENTUALLY -> check xpaths & add to channel
-						
-						// adding to store
-						dbInstance.putDocument(url, docTemp);
-						dbInstance.updateTime(url, currentDate.getTime());
-					}
-				
-				// if content isn't crawlable
+				// opening up a socket for a GET request
+				Socket getS = new Socket(currentURL.getHostName(), currentURL.getPortNo());
+				PrintWriter getOutput = new PrintWriter(getS.getOutputStream());
+				// send another request
+				getOutput.println("GET " + currentURL.getFilePath() + " HTTP/1.1");
+				getOutput.println("Host: localhost");
+				getOutput.println("User-Agent: cis455crawler");
+				getOutput.println("");
+				getOutput.flush();
+				// Setting up JTidy
+				Tidy domParse = new Tidy();
+				domParse.setForceOutput(true);
+				domParse.setShowErrors(0);
+				domParse.setQuiet(true);
+				boolean isHTML = !contentType.contains("xml");
+				// HTML Parsing
+				Document docTemp = null;
+				// XML parsing
+				DocumentBuilderFactory builder = DocumentBuilderFactory.newInstance();
+				DocumentBuilder dom = null;
+				// parsing HTML
+				System.out.println("Downloading: " + url);
+				if (isHTML) {
+					docTemp = domParse.parseDOM(new BufferedReader(
+							new InputStreamReader(getS.getInputStream())), null);
+					// searching document
+					searchForUrls(docTemp);
+					// adding to store
+					System.out.println("Finished processing: " + url);
+					System.out.println("");
+					dbInstance.putDocument(url, docTemp);
+					dbInstance.updateTime(url, currentDate.getTime());
+				// parsing XML
 				} else {
-					return;
+					try {
+						dom = builder.newDocumentBuilder();
+						docTemp = dom.parse(getS.getInputStream());
+					} catch (ParserConfigurationException e) {
+						e.printStackTrace();
+					} catch (SAXException e) {
+						e.printStackTrace();
+					}
+
+					// EVENTUALLY -> check xpaths & add to channel
+
+					// adding to store
+					System.out.println("Finished processing: " + url);
+					System.out.println("");
+					dbInstance.putDocument(url, docTemp);
+					dbInstance.updateTime(url, currentDate.getTime());
 				}
 			}
 		}
@@ -232,7 +217,7 @@ public class XPathCrawler {
 	 * @return true if content length is short enough, false if not
 	 */
 	private static boolean checkSizeHttp(String res) {
-		return true;
+		return maxSize >= Integer.parseInt(res.split(" ")[1]);
 	}
 	
 	/**
@@ -257,38 +242,32 @@ public class XPathCrawler {
 		int contentLength = headConnect.getContentLength();
 		long lastModified = headConnect.getLastModified();
 		String contentType = headConnect.getContentType();
-
 		// disconnect after getting relevant info
 		headConnect.disconnect();
-		
+		System.out.println("Checking crawlability of: " + url);
 		// if content type is appropriate, it's small enough, and has been modified since last crawled
 		if (checkContentTypeHttps(contentType) && 
 				checkSizeHttps(contentLength) && checkModifiedHttps(url, lastModified)) {
-			
 			// open up a new connection
 			HttpsURLConnection getConnect = (HttpsURLConnection) new URL(url).openConnection();
 			getConnect.setRequestMethod("GET");
-			
 			// Setting up JTidy
 			Tidy domParse = new Tidy();
 			domParse.setForceOutput(true);
 			domParse.setShowErrors(0);
 			domParse.setQuiet(true);
 			boolean isHTML = true;
-			
 			// Checking if html or xml
 			if (getConnect.getContentType() != null) {
 				isHTML = !getConnect.getContentType().endsWith("xml");
 			}
-			
 			// HTML Parsing
 			Document docTemp = null;
-			
 			// XML parsing
 			DocumentBuilderFactory builder = DocumentBuilderFactory.newInstance();
 			DocumentBuilder	dom = null;
-			
 			// parsing HTML
+			System.out.println("Downloading: " + url);
 			if (isHTML) {
 				try {
 					docTemp = domParse.parseDOM(getConnect.getInputStream(), null);
@@ -297,14 +276,13 @@ public class XPathCrawler {
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
-				
 				// searching document
 				searchForUrls(docTemp);
-				
 				// adding to store
+				System.out.println("Finished processing: " + url);
+				System.out.println("");
 				dbInstance.putDocument(url, docTemp);
 				dbInstance.updateTime(url, currentDate.getTime());
-			
 			// parsing XML
 			} else {
 				try {
@@ -319,10 +297,11 @@ public class XPathCrawler {
 				// EVENTUALLY -> check xpaths & add to channel
 				
 				// adding to store
+				System.out.println("Finished processing: " + url);
+				System.out.println("");
 				dbInstance.putDocument(url, docTemp);
 				dbInstance.updateTime(url, currentDate.getTime());
 			}
-		
 		// if content isn't crawlable
 		} else {
 			return;
@@ -363,11 +342,7 @@ public class XPathCrawler {
 	 * @return true if content is recent enough, false if not
 	 */
 	private static boolean checkModifiedHttps(String url, long date) {
-		
-		// get DB's data
 		long lastModified = dbInstance.getLastAccess(url);
-		
-		// not in DB
 		if (lastModified == 0) {
 			return true;
 		} else {
@@ -401,7 +376,6 @@ public class XPathCrawler {
 				for (int i = 0; i < children.getLength(); i++) {
 					searchNode(children.item(i));
 				}
-				
 			} else {
 				if (isLinkNode(n)) {
 					addLink(n);
@@ -419,6 +393,10 @@ public class XPathCrawler {
 		return (equals(n.getLocalName(), "a"));
 	}
 	
+	/**
+	 * Method to add links within a node that is determined to contain a link
+	 * @param n - node from which to add a link
+	 */
 	private static void addLink(Node n) {
 		// get the linked url & the current host url
 		String linkedUrl = n.getAttributes().getNamedItem("href").getNodeValue();
@@ -427,17 +405,14 @@ public class XPathCrawler {
 		if ((!hostUrl.endsWith("/") && !(hostUrl.endsWith(".html") || hostUrl.endsWith(".xml")) && !hostUrl.contains("?"))) {
 			hostUrl += "/";
 		}
-		
 		// url needs reformatting
 		if (!(linkedUrl.startsWith("http://") || linkedUrl.startsWith("https://") || linkedUrl.startsWith("www."))) {
 			URLInfo currentInfo = new URLInfo(hostUrl);
-		
 			// linked url is relative off of current host
 			if (linkedUrl.startsWith("/")) {
 				String newUrl = currentProtocol + currentInfo.getHostName() + linkedUrl;
 				if (!previouslySearched.contains(newUrl) && !urlQueue.contains(newUrl))
 					urlQueue.add(newUrl);
-			
 			// linked url is relative off of current filepath
 			} else {
 				String currentPath = currentInfo.getFilePath().substring(0, currentInfo.getFilePath().lastIndexOf("/") + 1);
@@ -445,17 +420,22 @@ public class XPathCrawler {
 				if (!previouslySearched.contains(newUrl) && !urlQueue.contains(newUrl))
 					urlQueue.add(newUrl);
 			}
-		
 		// url does not need reformatting
 		} else {
 			urlQueue.add(linkedUrl);
 		}
 	}
 
+	/**
+	 * Prints message when crawling is finished
+	 */
 	private static void exit() {
 		System.out.println("Done crawling");
 	}
 
+	/**
+	 * Prints the command structure in case user enters commands wrong
+	 */
 	public static void usage() {
 		System.out.println("Execution: java Crawler <url> <database-location> <max file size> [# of files to retrieve]");
 		System.out.println("<url>                 the url to start the web crawl at");
@@ -464,6 +444,12 @@ public class XPathCrawler {
 		System.out.println("[# of files]          number of files to crawl before exiting the crawler");
 	}
 	
+	/**
+	 * Compares two strings char by char
+	 * @param s1 - 1st string to compare
+	 * @param s2 - 2nd string to compare
+	 * @return true if equal, false if not
+	 */
 	private static boolean equals(String s1, String s2) {
 		boolean ret = false;
 		int i = 0;
